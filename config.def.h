fa8070cb073cce0ad7e5266eec5c4f98d4808753
Pablo Hugen <pablohuggem@gmail.com>
2021-08-17 15:43:53 -0300

    Add centeredmaster patch

--- config.def.h
+++ config.def.h
@@ -47,6 +47,8 @@ static const Layout layouts[] = {
 	{ "[M]",      monocle },
 	{ "TTT",      bstack },
 	{ "===",      bstackhoriz },
+	{ "|M|",      centeredmaster },
+	{ ">M>",      centeredfloatingmaster },
 };
 
 /* key definitions */
